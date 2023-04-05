f8e555cbf2d4acc57d5fad0fbadaa2ea4a13184e
Aditya Vaishnav <firehudson@gmail.com>
2023-04-05 06:35:24 +0000

    Add sendgrid email

--- src/pages/api/sendgrid.api.js
+++ src/pages/api/sendgrid.api.js
@@ -11,8 +11,8 @@ async function sendEmail(req, res) {
     const body = qs.parse(req.body);
 
     await sendgrid.send({
-      to: 'salonikabra1100@gmail.com', // Your email where you'll receive emails
-      from: 'salonikabra1100@gmail.com', // your website email address here
+      to: 'atharva@techstuff.cloud', // Your email where you'll receive emails
+      from: 'atharva@techstuff.cloud', // your website email address here
       subject: `Portfolio, Message from ${body.fullname.value}`,
       html: `Sender email:${body.email.value}<br/> <h3>${body.message.value} </h3>`,
     });
