de3719f48043ffd9f80ef77d914316ee1430220c
firehudson <firehudson@gmail.com>
2024-12-28 16:10:34 +0530

    Update resume page styles

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -1,16 +1,31 @@
 import { PDFViewer } from 'components/PDFViewer';
-import { Post } from 'layouts/Post';
 import styles from './Resume.module.css';
+import { Meta } from 'components/Meta';
+import { Section } from 'components/Section';
+import { Heading } from 'components/Heading';
+import { DecoderText } from 'components/DecoderText';
+import { Button } from 'components/Button';
 
 export const Resume = () => {
   return (
-    <div className={styles.resume}>
-      <Post
+
+    <article className={styles.resume}>
+      <Meta
         title="Resume"
-        href="https://raw.githubusercontent.com/firehudson/Portfolio-Website/main/src/assets/AdityaVaishnav_Resume.pdf"
-      >
-        <PDFViewer resume={true} pdfLink="https://raw.githubusercontent.com/firehudson/Portfolio-Website/main/src/assets/AdityaVaishnav_Resume.pdf" />
-      </Post>
-    </div>
+        description="Skilled in modern web development with expertise in JavaScript, React, CSS, and Node.js. Proficient in building responsive user interfaces and developing efficient backend solutions. Dedicated to delivering high-quality, scalable applications and continuously enhancing technical knowledge."
+      />
+      <Section className={styles.content}>
+        <div className={styles.grid}>
+          <Heading level={3} as="h1">
+            <DecoderText text="Resume" />
+          </Heading>
+
+          <Button href={"https://raw.githubusercontent.com/firehudson/Portfolio-Website/main/src/assets/AdityaVaishnav_Resume.pdf"} target="_self">
+            Download CV
+          </Button>
+        </div>
+      </Section>
+      <PDFViewer resume={true} pdfLink="https://raw.githubusercontent.com/firehudson/Portfolio-Website/main/src/assets/AdityaVaishnav_Resume.pdf" />
+    </article>
   );
 };
